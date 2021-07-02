1775e1e574a4afff68ef19726eb47f3632be1506
AdryanaP <dryportuga@gmail.com>
2021-07-02 10:44:18 -0300

    identando código e retirando if com tempo inadequado

--- microondas-objeto.js
+++ microondas-objeto.js
@@ -1,29 +1,27 @@
 // Checkpoint 2 - Microondas com objeto
 
 let tempoComida = {
-    pipoca: 10,
-    macarrão: 8,
-    brigadeiro: 8,
-    carne: 15,
-    feijão: 12
-}
+  pipoca: 10,
+  macarrão: 8,
+  brigadeiro: 8,
+  carne: 15,
+  feijão: 12,
+};
 
 function microondas(prato, tempoPreparo) {
-    let tempoPadrao = tempoComida[prato];
-  
-    if (tempoPreparo >= 3 * tempoPadrao) {
-      return "KABUMMM";
-    } else if (tempoPreparo >= 2 * tempoPadrao) {
-      return "Comida queimada :/";
-    } else if (tempoPreparo < tempoPadrao) {
-      return "Tempo insuficiente";
-    } else if (tempoPreparo > tempoPadrao) {
-      return "Tempo não recomendado";
-    } else if (tempoPadrao === undefined) {
-      return "Prato inexistente";
-    } else {
-      return "Prato pronto, bom apetite!!!";
-    }
-  }
+  let tempoPadrao = tempoComida[prato];
 
-  console.log(microondas("carne", 32));
+  if (tempoPreparo >= 3 * tempoPadrao) {
+    return "KABUMMM";
+  } else if (tempoPreparo >= 2 * tempoPadrao) {
+    return "Comida queimada :/";
+  } else if (tempoPreparo < tempoPadrao) {
+    return "Tempo insuficiente";
+  } else if (tempoPadrao === undefined) {
+    return "Prato inexistente";
+  } else {
+    return "Prato pronto, bom apetite!!!";
+  }
+}
+
+console.log(microondas("carne", 10));
